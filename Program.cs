e8431b3837ec8d3b4f7225dd083db5f8012421af
Sergey Zhuravlev <Sergey4Zhuravlev@gmail.com>
2019-12-03 02:57:06 +0300

    Fixed indents

--- Program.cs
+++ Program.cs
@@ -8,61 +8,58 @@ using System.Collections.Concurrent;
 
 namespace kavLogThreatsStates
 {
-    class Program
+  class Program
+  {
+    static int Main(string[] args)
     {
-        static int Main(string[] args)
+      try
+      {
+        if (args.Length != 1 || (args[0] is var logPath && !File.Exists(logPath)))
         {
-			try
-            {
-                if (args.Length != 1 || (args[0] is var logPath && !File.Exists(logPath)))
-				{
-					Console.Error.WriteLine("First argument should be log file for analyze.");
-					return -1;
-				}
-				var lines = File.ReadLines(logPath);
-				var reactive = new Regex(@"^\d\d:\d\d:\d\d\.\d\d\d\t0x\w+\t\w+\t.+\[TI: 0x(\w+)] St:(\w+) N:(.*) M:[\w ]", RegexOptions.Compiled | RegexOptions.ECMAScript);
-				var Sts = new ConcurrentDictionary<string, UInt64>();
-				var StChange = new ConcurrentDictionary<string, List<string>>();
-				UInt64 StsCount = 0;
-				foreach (var line in lines)
-				{
-					var r = reactive.Match(line);
-					if (r.Success)
-					{
-						++StsCount;
-						foreach (Group g in r.Groups)
-							Console.WriteLine(g.Value);
-						var state = r.Groups[2].Value;
-						Sts.AddOrUpdate(state, 0, (k, agr) => agr + 1);
-						var treat = r.Groups[1].Value;
-						StChange.AddOrUpdate(treat, new List<string>(), (k, set) =>
-						{
-							if (/*set.Contains(state)*/Object.Equals(state, set.LastOrDefault()))
-								return set;
-							else
-								set.Add(state);
-							return set;
-						});
-					}
-				}
-				Console.WriteLine("================");
-				foreach (var St in Sts)
-					Console.WriteLine($"{St.Key}:{St.Value}");
-				Console.WriteLine($"StsCount: {StsCount}");
-				var data = StChange.GroupBy(sc => string.Join(" ", sc.Value)).ToDictionary(k => k.Key, v => string.Join(" ", v.Select(vv => vv.Key)));
-				foreach (var d in data)
-					Console.WriteLine($"{d.Key}: {d.Value}");
-				Console.WriteLine("Ended");
-			}
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine(ex);
-                return -1;
-            }
-            return 0;
+          Console.Error.WriteLine("First argument should be log file for analyze.");
+          return -1;
         }
+        var lines = File.ReadLines(logPath);
+        var reactive = new Regex(@"^\d\d:\d\d:\d\d\.\d\d\d\t0x\w+\t\w+\t.+\[TI: 0x(\w+)] St:(\w+) N:(.*) M:[\w ]", RegexOptions.Compiled | RegexOptions.ECMAScript);
+        var Sts = new ConcurrentDictionary<string, UInt64>();
+        var StChange = new ConcurrentDictionary<string, List<string>>();
+        UInt64 StsCount = 0;
+        foreach (var line in lines)
+        {
+          var r = reactive.Match(line);
+          if (r.Success)
+          {
+            ++StsCount;
+            foreach (Group g in r.Groups)
+              Console.WriteLine(g.Value);
+            var state = r.Groups[2].Value;
+            Sts.AddOrUpdate(state, 0, (k, agr) => agr + 1);
+            var treat = r.Groups[1].Value;
+            StChange.AddOrUpdate(treat, new List<string>(), (k, set) =>
+            {
+              if (/*set.Contains(state)*/Object.Equals(state, set.LastOrDefault()))
+                return set;
+              else
+                set.Add(state);
+              return set;
+            });
+          }
+        }
+        Console.WriteLine("================");
+        foreach (var St in Sts)
+          Console.WriteLine($"{St.Key}:{St.Value}");
+        Console.WriteLine($"StsCount: {StsCount}");
+        var data = StChange.GroupBy(sc => string.Join(" ", sc.Value)).ToDictionary(k => k.Key, v => string.Join(" ", v.Select(vv => vv.Key)));
+        foreach (var d in data)
+          Console.WriteLine($"{d.Key}: {d.Value}");
+        Console.WriteLine("Ended");
+      }
+      catch (Exception ex)
+      {
+        Console.Error.WriteLine(ex);
+        return -1;
+      }
+      return 0;
     }
+  }
 }
-
-
-
